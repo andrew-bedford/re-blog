032e99e08d67fecf81be2ad4c469b93952dadb2c
Andrew Bedford <email@andrew-bedford.ca>
2024-01-20 20:38:38 -0500

    Add keyboard shortcuts to go to previous/next blog post
    Currently setup to use the left arrow for previous (older) posts, right
    arrow for next (newer) posts.

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Re.Blog;
 using MudBlazor.Services;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHotKeys2();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
